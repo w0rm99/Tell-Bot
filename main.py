0676df6df28840d8a191b475d51bff8da6c03794
w0rm99 <risingsafety99@gmail.com>
2024-06-19 15:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import os
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
-os.system('pip install python-telegram-bot --upgrade')
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -19,23 +17,26 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         output1 = subprocess.check_output('ls', shell=True).decode('utf-8')
         print(output1)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=output1)
-    
+
     elif update.message.text == 'cd':
         output1 = subprocess.check_output('cd', shell=True).decode('utf-8')
         print(output1)
-    
+
     elif update.message.text == 'pwd':
         output1 = subprocess.check_output('pwd', shell=True).decode('utf-8')
         print(output1)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=output1)
-    
+
     else:
-        output2 = subprocess.check_output(update.message.text, shell=True).decode('utf-8')
-        print(output2)
-        if output2:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=output2)
-        else:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text='no output')
+        try:
+            output2 = subprocess.check_output(update.message.text, shell=True).decode('utf-8')
+            print(output2)
+            if output2:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=output2)
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text='no output')
+        except subprocess.CalledProcessError as e:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
 
 if __name__ == '__main__':
     # API Key goes here
@@ -47,6 +48,5 @@ if __name__ == '__main__':
 
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
-    
-    application.run_polling()
 
+    application.run_polling()
